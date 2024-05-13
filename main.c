137281c2e8165396d47c99d0dd9aedf8cc336cbf
mini crabik <agl.malcv@gmail.com>
2024-05-14 00:34:12 +0300

    add lexicographical order and test

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include "string/task/reverseWord.h"
 #include "string/task/replaceDigitToSpace.h"
 #include "string/task/replaceWordToWord.h"
+#include "string/task/lexicographicalOrder.h"
 
 int main() {
     test_1_remove_non_letters();
     test_2_remove_adjacent_equal_letters();
     test_3_reverse_word();
     test_4_replace_digit_to_space();
-    test_5_replace_word_to_word
+    test_5_replace_word_to_word();
+    test_6_lexicographical_order();
     return 0;
 }
