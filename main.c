c811d7eb59d9f25bf1e432581a2136edf763713e
mini crabik <agl.malcv@gmail.com>
2024-05-13 21:47:30 +0300

    add remove adjace entqual letters and test

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "string/task/removeNonLetters.h"
+#include "string/task/removeAdjacentEqualLetters.h"
 
 
 int main() {
     test_1_remove_non_letters();
+    test_2_remove_adjacent_equal_letters();
+
 
 
     return 0;
