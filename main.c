446b624fe2bf5dda6efb12adf1128a9ac59d4b2e
mini crabik <agl.malcv@gmail.com>
2024-05-13 23:42:43 +0300

    add replace word to word and test

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 #include "string/task/removeAdjacentEqualLetters.h"
 #include "string/task/reverseWord.h"
 #include "string/task/replaceDigitToSpace.h"
+#include "string/task/replaceWordToWord.h"
 
 int main() {
     test_1_remove_non_letters();
     test_2_remove_adjacent_equal_letters();
     test_3_reverse_word();
     test_4_replace_digit_to_space();
-
+    test_5_replace_word_to_word
     return 0;
 }
